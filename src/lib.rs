13a962d13600df6c0f848c037b1f89ee380f858c
Zademn <zademnwalves@gmail.com>
2022-02-15 08:56:32 +0200

    refactored

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub mod algorithms;
+pub mod dixon;
+pub mod fermat;
+pub mod quadratic_sieve;
+pub mod traits;
+pub mod trial_division;
+
+// pub use quadratic_sieve::*;
+// pub use trial_division::*;
+// pub use dixon::*;
+// pub use fermat::*;
+// pub use algorithms::*;
+// pub use quadratic_sieve::*;
